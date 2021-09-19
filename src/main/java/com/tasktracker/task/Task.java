f414b2503b20901c21f2e25e9fe5356eb07fee4c
Igor Nosovskyi <igor.nosovsky@temy.co>
2021-09-19 22:24:24 +0300

    add user and task POJO

---
+++ src/main/java/com/tasktracker/task/Task.java
@@ -0,0 +1,69 @@
+package com.tasktracker.task;
+
+public class Task {
+    private Long id;
+    private String title;
+    private String description;
+    private String reporter;
+    private String assignee;
+
+    public Task(Long id, String title, String description, String reporter, String assignee) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.reporter = reporter;
+        this.assignee = assignee;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getReporter() {
+        return reporter;
+    }
+
+    public void setReporter(String reporter) {
+        this.reporter = reporter;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Task{");
+        sb.append("id=").append(id);
+        sb.append(", title='").append(title).append('\'');
+        sb.append(", description='").append(description).append('\'');
+        sb.append(", reporter='").append(reporter).append('\'');
+        sb.append(", assignee='").append(assignee).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/tasktracker/user/User.java
@@ -0,0 +1,58 @@
+package com.tasktracker.user;
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private boolean deleted;
+
+    public User(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.deleted = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("User{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append(", deleted=").append(deleted);
+        sb.append('}');
+        return sb.toString();
+    }
+}
