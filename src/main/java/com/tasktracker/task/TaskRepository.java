c76e734ae4274b03c38c5eb44544968b6f13e8b0
Igor Nosovskyi <igor.nosovsky@temy.co>
2021-10-09 19:37:13 +0300

    move configuration

--- src/main/java/com/tasktracker/task/TaskRepository.java
+++ src/main/java/com/tasktracker/task/TaskRepository.java
@@ -1,11 +1,24 @@
 package com.tasktracker.task;
 
+import com.tasktracker.common.CommonDAO;
+import com.tasktracker.user.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
-public class TaskRepository implements TaskDao {
+public class TaskRepository implements CommonDAO<Task> {
+    private final AtomicLong aLong;
+    private final Map<Long, Task> tasks;
+
+    public TaskRepository(AtomicLong aLong, Map<Long, Task> tasks) {
+        this.aLong = aLong;
+        this.tasks = tasks;
+    }
+
+
     @Override
     public Task create(Task task) {
         return null;
